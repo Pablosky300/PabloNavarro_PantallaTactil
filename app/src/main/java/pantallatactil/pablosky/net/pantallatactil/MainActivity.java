505331f7b0f39a21daabd581df6486d579e7ca99
Pablosky <pablo160896@gmail.com>
2016-12-29 17:04:58 +0100

    Practica 5.9 Acabada

--- app/src/main/java/pantallatactil/pablosky/net/pantallatactil/MainActivity.java
+++ app/src/main/java/pantallatactil/pablosky/net/pantallatactil/MainActivity.java
@@ -2,12 +2,46 @@ package pantallatactil.pablosky.net.pantallatactil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView entrada = (TextView)findViewById(R.id.TextViewEntrada);
+        entrada.setOnTouchListener(this);
+
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        TextView salida = (TextView)findViewById(R.id.TextViewSalida);
+
+        Float presion = motionEvent.getPressure();
+        Float tam = motionEvent.getSize();
+
+        salida.append("\nPresión: " + presion + "\n" + "Tamaño: " + tam + "\n");
+//salida.append("\n" + motionEvent.toString()+"\n" );
+        String acciones[] = { "ACTION_DOWN", "ACTION_UP", "ACTION_MOVE",
+                "ACTION_CANCEL", "ACTION_OUTSIDE", "ACTION_POINTER_DOWN",
+                "ACTION_POINTER_UP" };
+        int accion = motionEvent.getAction();
+        int codigoAccion = accion & MotionEvent.ACTION_MASK;
+        int iPuntero = (accion & MotionEvent.ACTION_POINTER_INDEX_MASK)
+                >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+        salida.append("\n" + acciones[codigoAccion]);
+        salida.append(" Generada por puntero: " + iPuntero);
+        for (int i = 0; i < motionEvent.getPointerCount(); i++) {
+            salida.append("\npuntero:" + motionEvent.getPointerId(i) +
+                    " x:" + motionEvent.getX(i) + " y:" + motionEvent.getY(i));
+        }
+        salida.append("\n");
+
+        return true;
     }
 }
